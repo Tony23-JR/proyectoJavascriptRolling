00faed7f4be8af1ce0d9ad09039fe1db9ed892c1
DESKTOP-0NQPCTD\arotg.sabra <rotgerantonio@gmail.com>
2020-10-02 20:31:20 -0300

    Version 2

---
+++ js/book.models.js
@@ -0,0 +1,11 @@
+class Book {
+    constructor(id, title, description, date, author, image) {
+      this.id = id;
+      this.title = title;
+      this.description = description;
+      this.date = date;
+      this.author = author;
+      this.genere = genere;
+      this.image = image;
+    }
+  }
---
+++ js/localStorageManager.js
@@ -0,0 +1,19 @@
+class LocalStorageManager 
+{
+    _localStorage;
+    
+    constructor(localStorage) {
+        this._localStorage = localStorage;
+    }
+    
+    get(keyStorage) 
+    {
+        let users = JSON.parse(this._localStorage.getItem(keyStorage)) || [];
+        return users;
+    }
+    
+    set(keyStorage, models)
+    {
+        this._localStorage.setItem(keyStorage, JSON.stringify(models))
+    }
+}
